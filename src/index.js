3089b2cf781809659f55a555ff31091da700e54e
SylwiaOlejko <63730148+SylwiaOlejko@users.noreply.github.com>
2020-12-14 04:53:14 +0100

    latitude and longitude

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function reteirvePosition(position){
+
+let latitude = position.coords.latitude;
+let longitude = position.coords.latitude;
+
+
+}
+
+navigator.geolocation.getCurrentPosition(reteirvePosition);
