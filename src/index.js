8ceeb7a1bc0bde588b299a0e686bbd4d71735c76
SylwiaOlejko <63730148+SylwiaOlejko@users.noreply.github.com>
2020-12-14 05:07:27 +0100

    temperature

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ function reteirvePosition(position){
 
 let latitude = position.coords.latitude;
 let longitude = position.coords.latitude;
+let apiKey = "4a4fe35e6c00e25f675f5b029f252096";
+let units = "metric";
 
 
+let apiUrl = 'http://api.openweathermap.org/data/2.5/find?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}';
+console.log(apiUrl);
 }
-
 navigator.geolocation.getCurrentPosition(reteirvePosition);
