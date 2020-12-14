115560ecae23bb2e504a725fca538aa248aed2c3
SylwiaOlejko <63730148+SylwiaOlejko@users.noreply.github.com>
2020-12-14 05:17:01 +0100

    showTemperature

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+function showTemperature( response){
+
+console.log(response.data.main.temp);
+let temperature = Math.round(response.data.main.temp);
+
+let heading= document.querySelector("h1");
+heading.innerHtml ='The outside is ${temperature}C';
+
+
+}
+
+
 function reteirvePosition(position){
 
 let latitude = position.coords.latitude;
 let longitude = position.coords.latitude;
 let apiKey = "4a4fe35e6c00e25f675f5b029f252096";
 let units = "metric";
+let apiEndpoint = "http://api.openweathermap.org/data/2.5/weather ";
 
+let apiUrl = '${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}';
 
-let apiUrl = 'http://api.openweathermap.org/data/2.5/find?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}';
-console.log(apiUrl);
+axios.get(apiUrl).then(showTemperature);
+
 }
 navigator.geolocation.getCurrentPosition(reteirvePosition);
